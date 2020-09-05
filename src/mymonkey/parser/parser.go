621c9943dcff24fac246f9a3ebf50be430acb9ab
JamilDroubi <jawsomeone@hotmail.com>
2020-09-04 18:34:41 -0700

    page 60

--- src/mymonkey/parser/parser.go
+++ src/mymonkey/parser/parser.go
@@ -5,6 +5,18 @@ import (
     "mymonkey/lexer"
     "mymonkey/token"
     "fmt"
+    "strconv"
+)
+
+const (
+    _ int = iota
+    LOWEST
+    EQUALS      // ==
+    LESSGREATER // > or <
+    SUM // +
+    PRODUCT // *
+    PREFIX // -X or !X
+    CALL // myFunction(X)
 )
 
 type (
@@ -31,8 +43,15 @@ func New(l *lexer.Lexer) *Parser {
     /* Read two tokens, so curToken and peekToken are both set */
     p.nextToken()
     p.nextToken()
+    p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+    p.registerPrefix(token.IDENT, p.parseIdentifier)
+    p.registerPrefix(token.INT,   p.parseIntegerLiteral)
 
     return p
+}
+
+func (p *Parser) parseIdentifier() ast.Expression {
+    return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 }
 
 func (p *Parser) Errors() []string {
@@ -68,7 +87,7 @@ func (p *Parser) parseStatement() ast.Statement {
     switch p.curToken.Type {
         case token.LET    : return p.parseLetStatement()
         case token.RETURN : return p.parseReturnStatement()
-        default           : return nil
+        default           : return p.parseExpressionStatement()
     }
 }
 
@@ -111,7 +130,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
     p.nextToken()
 
-    // TODO: We're skippingthe expressions until we
+    // TODO: We're skipping the expressions until we
     // encounter a semicolon
 
     for !p.curTokenIs(token.SEMICOLON) {
@@ -127,4 +146,41 @@ func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
 
 func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
     p.infixParseFns[tokenType] = fn
+}
+
+func (p *Parser) parseExpression(precedence int) ast.Expression {
+    prefix := p.prefixParseFns[p.curToken.Type]
+    if prefix == nil {
+        return nil
+    }
+    leftExp := prefix()
+
+    return leftExp
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+    stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+    stmt.Expression = p.parseExpression(LOWEST)
+
+    if p.peekTokenIs(token.SEMICOLON) {
+        p.nextToken()
+    }
+
+    return stmt
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+    lit := &ast.IntegerLiteral{Token: p.curToken}
+
+    value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+    if err != nil {
+        msg := fmt.Sprintf("could not parse %q as an intege", p.curToken.Literal)
+        p.errors = append(p.errors, msg)
+        return nil
+    }
+
+    lit.Value = value
+
+    return lit
 }
